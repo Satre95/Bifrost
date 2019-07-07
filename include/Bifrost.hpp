2264d04affe4d920206edfe77c9b41e6b28ca718
Siddharth Atre <sidatre@gmail.com>
2019-07-06 17:18:56 -0700

    Formatting.

--- include/Bifrost.hpp
+++ include/Bifrost.hpp
@@ -4,20 +4,24 @@
 #include <unordered_map>
 #include <vector>
 
-template <class Key, class Value> class Bifrost {
+template <class Key, class Value>
+class Bifrost
+{
 public:
   Bifrost(const size_t numBuckets, const float cellSize);
 
   void Insert(const Key &aKey, Value &aValue);
 
-  void Neighbors(const Key &aKey, std::vector<Value> & neighborList);
+  void Neighbors(const Key &aKey, std::vector<Value> &neighborList);
 
   void Clear() { mHashtable.clear(); }
 
 private:
-  struct DiscreteKey3D {
+  struct DiscreteKey3D
+  {
     DiscreteKey3D() { keys[0] = keys[1] = keys[2] = 0; }
-    DiscreteKey3D(int i, int j, int k) {
+    DiscreteKey3D(int i, int j, int k)
+    {
       keys[0] = i;
       keys[1] = j;
       keys[2] = k;
@@ -25,11 +29,13 @@ private:
     int keys[3];
     int &operator[](int i) { return keys[i]; }
     const int &operator[](int i) const { return keys[i]; }
-    bool operator==(const DiscreteKey3D & rhs) const {
+    bool operator==(const DiscreteKey3D &rhs) const
+    {
       return this->keys[0] == rhs.keys[0] && this->keys[1] == rhs.keys[1] && this->keys[2] == rhs.keys[2];
     }
 
-    DiscreteKey3D operator+(const DiscreteKey3D &rhs) {
+    DiscreteKey3D operator+(const DiscreteKey3D &rhs)
+    {
       DiscreteKey3D sum;
       sum.keys[0] = this->keys[0] + rhs.keys[0];
       sum.keys[1] = this->keys[1] + rhs.keys[1];
@@ -37,9 +43,11 @@ private:
       return sum;
     }
   };
-  
-  struct SpatialHash : std::unary_function<DiscreteKey3D, size_t> {
-    size_t operator()(const DiscreteKey3D pos) const {
+
+  struct SpatialHash : std::unary_function<DiscreteKey3D, size_t>
+  {
+    size_t operator()(const DiscreteKey3D pos) const
+    {
       const unsigned int prime1 = 73856093;
       const unsigned int prime2 = 19349663;
       const unsigned int prime3 = 83492791;
@@ -47,7 +55,8 @@ private:
     }
   };
 
-  DiscreteKey3D Discretize(const Key &key) const {
+  DiscreteKey3D Discretize(const Key &key) const
+  {
     DiscreteKey3D dKey;
     dKey[0] = static_cast<int>(std::floor(key[0] * mInverseCellSize));
     dKey[1] = static_cast<int>(std::floor(key[1] * mInverseCellSize));
@@ -63,7 +72,8 @@ private:
 
 template <class Key, class Value>
 Bifrost<Key, Value>::Bifrost(const size_t numBuckets, const float cellSize)
-    : mHashtable(numBuckets), mInverseCellSize(1.f / cellSize) {
+    : mHashtable(numBuckets), mInverseCellSize(1.f / cellSize)
+{
   for (int i = -1; i <= 1; i++)
     for (int j = -1; j <= 1; j++)
       for (int k = -1; k <= 1; k++)
@@ -71,17 +81,21 @@ Bifrost<Key, Value>::Bifrost(const size_t numBuckets, const float cellSize)
 }
 
 template <class Key, class Value>
-void Bifrost<Key, Value>::Insert(const Key &aKey, Value &aValue) {
+void Bifrost<Key, Value>::Insert(const Key &aKey, Value &aValue)
+{
   mHashtable[Discretize(aKey)].push_back(aValue);
 }
 
 template <class Key, class Value>
 void Bifrost<Key, Value>::Neighbors(const Key &aKey,
-                                    std::vector<Value> & neighborList) {
+                                    std::vector<Value> &neighborList)
+{
   auto dKey = Discretize(aKey);
-  for (const auto &anOffset : mOffsets) {
+  for (const auto &anOffset : mOffsets)
+  {
     auto it = mHashtable.find(dKey + anOffset);
-    if (it != mHashtable.end()) {
+    if (it != mHashtable.end())
+    {
       neighborList.insert(neighborList.end(), it->second.begin(),
                           it->second.end());
     }
